d0818f290f351bfb851326c5784127ec7afe9926
Paillat-dev <me@paillat.dev>
2026-02-28 13:07:19 +0100

    :recycle: Migrate copyright metadata from `.copywrite.hcl` to `licensor-config.yaml`, update dependencies in `uv.lock`, and add SPDX license notices.

--- src/parsers/__init__.py
+++ src/parsers/__init__.py
@@ -1,8 +1,12 @@
-# Copyright (c) Paillat-dev
 # SPDX-License-Identifier: MIT
+# Copyright: 2024-2026 Paillat-dev
 
-from .base import BuildParser
+from typing import TYPE_CHECKING
+
 from .emojis_parser import EmojisParser
+
+if TYPE_CHECKING:
+    from .base import BuildParser
 
 PARSERS: list[type[BuildParser]] = [EmojisParser]
 
